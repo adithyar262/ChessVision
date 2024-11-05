a5e9b0a16e4450441a9d2426ca1709d6f842e50e
Adithya Rajeev <adithyar262@gmail.com>
2024-11-05 11:25:48 -0500

    [UI] Added piece position map

--- src/main.hpp
+++ src/main.hpp
@@ -3,6 +3,27 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <string>
+#include <unordered_map>
+
+struct TextureCoord {
+    float u, v;
+};
+
+std::unordered_map<char, TextureCoord> pieceToTexture = {
+    {'R', {0.0f, 0.0f}},   // White Rook
+    {'N', {0.125f, 0.0f}}, // White Knight
+    {'B', {0.25f, 0.0f}},  // White Bishop
+    {'Q', {0.375f, 0.0f}}, // White Queen
+    {'K', {0.5f, 0.0f}},   // White King
+    {'P', {0.0f, 0.125f}}, // White Pawn
+    {'r', {0.0f, 0.875f}},   // Black Rook
+    {'n', {0.125f, 0.875f}}, // Black Knight
+    {'b', {0.25f, 0.875f}},  // Black Bishop
+    {'q', {0.375f, 0.875f}}, // Black Queen
+    {'k', {0.5f, 0.875f}},   // Black King
+    {'p', {0.625f, 0.75f}},  // Black Pawn
+    {' ', {0.75f, 0.5f}}   // Empty square
+};
 
 class Shader {
 public:
